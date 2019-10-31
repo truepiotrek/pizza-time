f12fb218825dd7d73aa44ec3ef20cad0a4aa88de
Peter Slezak <piotrek.slezak@gmail.com>
2019-10-31 16:29:55 +0100

    created initMenu and initData method

--- src/js/script.js
+++ src/js/script.js
@@ -52,10 +52,31 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(){
+      const thisProduct = this;
 
+      console.log('new Product', thisProduct);
+    }
+  }
 
   
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+
+      console.log('thisApp.data:', thisApp.data);
+
+      const testProduct = new Product();
+      console.log('testProduct', testProduct);
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -63,8 +84,11 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
-
+  
   app.init();
 }
