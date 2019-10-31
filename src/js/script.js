1e9684fdf854cceea4eebb96461091929a157d01
Peter Slezak <piotrek.slezak@gmail.com>
2019-10-31 16:55:36 +0100

    rendered products on site

--- src/js/script.js
+++ src/js/script.js
@@ -53,12 +53,29 @@
   };
 
   class Product{
-    constructor(){
+    constructor(id, data){
       const thisProduct = this;
 
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
       console.log('new Product', thisProduct);
     }
+
+    renderInMenu(){
+      const thisProduct = this;
+      // generate hmtl based on template
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      // create element using utils.createElementFromHTML
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      // find menu container
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      // add element to menu
+      menuContainer.appendChild(thisProduct.element);
+    }
   }
+
 
   
   const app = {
@@ -67,8 +84,9 @@
 
       console.log('thisApp.data:', thisApp.data);
 
-      const testProduct = new Product();
-      console.log('testProduct', testProduct);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
     },
 
     initData: function(){
