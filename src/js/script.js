874ad87bb76c978296b87c21b2896b6d738b9b9e
Peter Slezak <piotrek.slezak@gmail.com>
2019-11-09 18:34:30 +0100

    added structure for price counting

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,8 @@
       thisProduct.renderInMenu();
       console.log('new Product', thisProduct);
 
+      thisProduct.getElements();
+
       thisProduct.initAccordion();
     }
 
@@ -75,6 +77,16 @@
       const menuContainer = document.querySelector(select.containerOf.menu);
       // add element to menu
       menuContainer.appendChild(thisProduct.element);
+    }
+
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
     }
 
     initAccordion(){
